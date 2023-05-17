d9d2796f2ff2223b87a696fa768b6aa04501b84d
jungbin-lee <hihihi486@naver.com>
2023-05-17 17:52:41 +0900

    add position

--- app/src/main/java/com/h2square/viewpagerpractice/adapters/NameViewPagerAdapter.kt
+++ app/src/main/java/com/h2square/viewpagerpractice/adapters/NameViewPagerAdapter.kt
@@ -10,6 +10,16 @@ import com.h2square.viewpagerpractice.fragments.NameFragment
 class NameViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm){
 
 
+    override fun getPageTitle(position: Int): CharSequence? {
+        if (position==0){
+            return "이름"
+        }else if (position==1){
+            return "내정보"
+        }else{
+            return "인사"
+        }
+    }
+
     override fun getItem(position: Int): Fragment {
     if (position==0){
         return NameFragment()
